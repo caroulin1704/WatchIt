34a65d2399f7512330353a16e02cddfeb464c8ab
caroulin1704 <carod.emeraude@gmail.com>
2023-03-14 16:57:14 +0100

    Création des packages, classes et repositories

--- WatchIt/src/main/java/fr/solutec/watchit/WatchItApplication.java
+++ WatchIt/src/main/java/fr/solutec/watchit/WatchItApplication.java
@@ -1,13 +1,19 @@
 package fr.solutec.watchit;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WatchItApplication {
+public class WatchItApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WatchItApplication.class, args);
+		System.out.println("Lancement terminé");
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Lancement en cours");
 	}
 
 }
