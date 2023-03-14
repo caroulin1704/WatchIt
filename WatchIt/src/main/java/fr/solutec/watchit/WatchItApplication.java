017a324df7b38702f3d8d967e7780513cd9e3c04
caroulin1704 <carod.emeraude@gmail.com>
2023-03-14 15:08:24 +0100

    Création git repository

---
+++ WatchIt/src/main/java/fr/solutec/watchit/WatchItApplication.java
@@ -0,0 +1,13 @@
+package fr.solutec.watchit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchItApplication.class, args);
+	}
+
+}
