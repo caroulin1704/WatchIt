327fbb14effc5595e8819490463b2558ef547726
caroulin1704 <carod.emeraude@gmail.com>
2023-03-15 15:16:30 +0100

    Création des APIs de base pour User

--- WatchIt/src/main/java/fr/solutec/repository/UserRepository.java
+++ WatchIt/src/main/java/fr/solutec/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package fr.solutec.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+	
+	public Optional<User> findByPseudoAndPassword(String pseudo, String password);
 }
--- WatchIt/src/main/java/fr/solutec/rest/UserRest.java
+++ WatchIt/src/main/java/fr/solutec/rest/UserRest.java
@@ -1,5 +1,48 @@
 package fr.solutec.rest;
 
-public class UserRest {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.entities.User;
+import fr.solutec.repository.UserRepository;
+
+@RestController
+@CrossOrigin("*")
+public class UserRest {
+	@Autowired
+	private UserRepository userRepos;
+	
+	@PostMapping("login")
+	public Optional<User> connexion(@RequestBody User u) {
+		return userRepos.findByPseudoAndPassword(u.getPseudo(), u.getPassword());
+	}
+	
+	@PostMapping("inscription")
+	public User inscrireUser(@RequestBody User u) {
+		return userRepos.save(u);
+	}
+	
+	@DeleteMapping("user/{id}")
+	public boolean suppUser(@PathVariable Long id) {
+		Optional<User> u = userRepos.findById(id);
+		if(u.isPresent()) {
+			userRepos.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+	
+	@PutMapping("user/{id}")
+	public User modifUser(@RequestBody User u, @PathVariable Long id) {
+		u.setId(id);
+		return userRepos.save(u);
+	}
 }
